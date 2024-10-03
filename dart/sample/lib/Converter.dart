801441b860954d3cc9095ee62c996aea7cf979d2
Alan Lai <alanlai@Alans-MacBook-Pro-3.local>
2024-10-02 19:26:45 -0700

    PR #1: Handle single digits

--- dart/sample/lib/Converter.dart
+++ dart/sample/lib/Converter.dart
@@ -1,5 +1,13 @@
 
 String integerToWordedString(int number) {
-  return "";
+  if(0 <= number && number <= 9) {
+    return _getSingleDigitNumberAsString(number);
+  } else {
+    throw UnimplementedError("No support for #s > 9!");
+  }
 }
 
+String _getSingleDigitNumberAsString(int number) {
+  var singleDigitNumsAsString = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+  return singleDigitNumsAsString[number];
+}
